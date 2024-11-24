ba7f113f2cc7de48977288579f984c8cc57c9f34
AcerCo <Elijahsaurusrex0702@gmail.com>
2024-11-23 20:48:36 -0600

    readme and ba bye vercel

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rampant</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="icon" href="assets/favicon/favicon.png">
+</head>
+<body>
+
+    <h1 align="center">Rampant</h1>
+    <p align="center">Rampant is a site that has the most popular lists in GD <br> (Demon List, ILL, PPLL, Challange List & SLL).
+
+    <hr style="margin-top: 50px; margin-bottom: 50px;">
+
+    <h1 align="center">Lists:</h1>
+    <a href="demon" style="color: transparent;"><button>Demon List</button></a>
+    <button>ILL</button>
+    <button>PPLL</button>
+
+    <hr>
+
+    <h1 align="center">Other:</h1>
+    <p align="center">Note: Rampant is currently under developement and will have much more features later on. <br><br> Check out the github! </p>
+
+    <a href="https://github.com/GMDPLUTONIC/Rampant">
+    <img src="assets/img/github-mark.png" alt="" style="height: 7.5em; width: 7.5em; margin: auto 555px;">
+</body>
+</html>
